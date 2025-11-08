bef28d0be8f1fd1dfa80762d3da4630d8e7f3951
chandrasekhar <chandrasekhar.r@krelixir.us>
2025-11-08 15:20:19 +0530

    feature/wallet-cr : update wallet controller

--- src/main/java/com/payment/epurse/controller/WalletController.java
+++ src/main/java/com/payment/epurse/controller/WalletController.java
@@ -1,4 +1,18 @@
 package com.payment.epurse.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/epurse/wallet")
 public class WalletController {
+
+    @GetMapping("/getWalletDetails/{custId}")
+    public ResponseEntity<?>  getWalletDetails(@PathVariable Long custId){
+        return new ResponseEntity<>(HttpStatusCode.valueOf(HttpStatus.OK.value()));
+    }
+
 }
