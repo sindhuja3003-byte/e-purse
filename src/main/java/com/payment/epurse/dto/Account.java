882b3e4a54940839ec68b868259c571e06b116d8
RajeshJavaDevelpoer <er.rajcse@yahoo.in>
2025-11-08 16:00:31 +0530

    Add JPA annotations and builder pattern to Account class

--- src/main/java/com/payment/epurse/dto/Account.java
+++ src/main/java/com/payment/epurse/dto/Account.java
@@ -1,4 +1,21 @@
 package com.payment.epurse.dto;
+@Entity
+@Table(name = "account")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+  public class Account { 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long accountId;
 
-public class Account {
+    private String accountNumber;
+    private Long customerId;
+    private Double balance;
+    private Double debit;
+    private Double credit;
+    private String currency;
+    private LocalDateTime date;
+}
 }
