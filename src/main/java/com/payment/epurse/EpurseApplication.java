ce9769eb77ce7d2161fa47aab431eae454f53fde
sindhuja3003@gmail.com <sindhuja3003@gmail.com>
2025-11-08 14:12:20 +0530

    Initial commit

---
+++ src/main/java/com/payment/epurse/EpurseApplication.java
@@ -0,0 +1,13 @@
+package com.payment.epurse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpurseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpurseApplication.class, args);
+	}
+
+}
